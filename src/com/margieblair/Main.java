8b7705ad3bf20783a2544c5b32fda6c292eaea89
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-04 17:30:47 -0500

    successfully printed out a full deck of 52 cards with four distinct suits

--- src/com/margieblair/Main.java
+++ src/com/margieblair/Main.java
@@ -9,7 +9,7 @@ public class Main {
         DeckOfCards deck = new DeckOfCards();
         for (int i = 0; i < deck.cards.length; i++) {
             for (int j = 0; j < deck.cards[i].length; j++) {
-                System.out.println((deck.cards[i][j]).displayCard());
+                (deck.cards[i][j]).displayCard();
             }
         }
 
