653549bb36f763198c18b7778b7362dbee623e4f
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-04 14:48:38 -0500

    tweaked it to iterate through enum

--- src/com/margieblair/DeckOfCards.java
+++ src/com/margieblair/DeckOfCards.java
@@ -5,9 +5,10 @@ public class DeckOfCards {
 
     public DeckOfCards() {
         this.cards = new Card[52];
-        for (int i = 0; i < 52; i++) {
-            Card card = new Card(); //instantiate the card here
-
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                Card card = new Card(Suit.values()[i], Ranks.values()[j]); //instantiate the card here
+            }
         }
     }
 }
