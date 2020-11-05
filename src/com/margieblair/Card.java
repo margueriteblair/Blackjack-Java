c58b2093fe8e0a57eff9d56e98f647404f70fc86
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-04 20:55:59 -0500

    created a switch statement for the card ranks to return an integer value

--- src/com/margieblair/Card.java
+++ src/com/margieblair/Card.java
@@ -13,5 +13,45 @@ public class Card {
         System.out.println(suit + " " + rank);
     }
 
+    public int returnCardValue() {
+        switch(rank) {
+            case ACE -> {
+                return 1;
+            }
+            case TWO -> {
+                return 2;
+            }
+            case THREE -> {
+                return 3;
+            }
+            case FOUR -> {
+                return 4;
+            }
+            case FIVE -> {
+                return 5;
+            }
+            case SIX -> {
+                return 6;
+            }
+            case SEVEN -> {
+                return 7;
+            }
+            case EIGHT -> {
+                return 8;
+            }
+            case NINE -> {
+                return 9;
+            }
+            case TEN, JACK, QUEEN, KING -> {
+                return 10;
+                //you use a comma to separate the different cases that would result in the same value
+            }
+            default -> {
+                return 0;
+            }
+
+        }
+    }
+
 
 }
