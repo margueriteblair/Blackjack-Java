702c9ccba96fb3f0e95adcfc4421f1a5dadeca35
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-05 23:31:17 -0500

    created points var in hand class

--- src/com/margieblair/Hand.java
+++ src/com/margieblair/Hand.java
@@ -3,6 +3,7 @@ package com.margieblair;
 public abstract class Hand {
     private String name;
     private int bet;
+    private int points;
 
     public Hand(String name, int bet) {
         this.name = name;
@@ -10,5 +11,14 @@ public abstract class Hand {
     }
 
     public void draw() {}
+
+    public void displayPlayerTotals() {
+//        var playerTotal = playerCards.stream().mapToInt(i -> i).sum();
+//        var cpuTotal = cpuCards.stream().mapToInt(i -> i).sum();
+//        System.out.println("Player, your total right now is " + playerTotal + " while CPU total is " + cpuTotal);
+//
+//        cpuTotalPoints = cpuTotal;
+//        player1Total = playerTotal;
+    }
 
 }
