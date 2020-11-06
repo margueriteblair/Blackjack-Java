420146b5edd33416c6ed1519d9dc5b48b8e722bc
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-05 23:39:19 -0500

    points and cleanup

--- src/com/margieblair/Player.java
+++ src/com/margieblair/Player.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class Player extends Hand{
     private List<Integer> playerCards = new ArrayList<>();
-    private int player1Total;
+    private int points;
+    private String name;
 
     public Player(String name, int bet) {
         super(name, bet);
@@ -17,12 +18,18 @@ public class Player extends Hand{
 //            System.out.println("No more cards to draw");
 //            return;
 //        }
-        for (int i = 0; i < numDraw; i++) {
-            cards.get(0).displayCard();
-            if (BlackjackConsole.getPlayer().equals("Player")) {
-                playerCards.add(cards.get(0).returnCardValue());
-            }
-            cards.remove(0);
-        }
+//        for (int i = 0; i < numDraw; i++) {
+//            cards.get(0).displayCard();
+//            if (BlackjackConsole.getPlayer().equals("Player")) {
+//                playerCards.add(cards.get(0).returnCardValue());
+//            }
+//            cards.remove(0);
+//        }
+    }
+
+    public void displayPoints() {
+        var total = playerCards.stream().mapToInt(i -> i).sum();
+        System.out.println(name + " your total right now is " + total + " .");
+        points = total;
     }
 }
