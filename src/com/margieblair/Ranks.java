d28991534d559453261b23f334f4da433208aa36
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 21:16:09 -0500

    pushing changes

--- src/com/margieblair/Ranks.java
+++ src/com/margieblair/Ranks.java
@@ -3,6 +3,7 @@ package com.margieblair;
 public enum Ranks {
     //HI CLIFF, LOL I'M WRITING THIS RIGHT AFTER YOU SAID AVOID RANKS/VALUES
     //AS ENUM, DULY NOTED I WILL REFACTOR THIS :D
+    //I just wanted the words to display but yeah, lolz, will change!
     ACE,
     TWO,
     THREE,
