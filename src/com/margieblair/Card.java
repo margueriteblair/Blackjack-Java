7fe6515acfb63f54cec578aaad2652d9f53d7a70
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-10 23:57:47 -0500

    deleted all the cases in the switch statement that aren't numbers

--- src/com/margieblair/Card.java
+++ src/com/margieblair/Card.java
@@ -28,30 +28,6 @@ public class Card {
                 }
 
             }
-            case TWO -> {
-                return 2;
-            }
-            case THREE -> {
-                return 3;
-            }
-            case FOUR -> {
-                return 4;
-            }
-            case FIVE -> {
-                return 5;
-            }
-            case SIX -> {
-                return 6;
-            }
-            case SEVEN -> {
-                return 7;
-            }
-            case EIGHT -> {
-                return 8;
-            }
-            case NINE -> {
-                return 9;
-            }
             case TEN, JACK, QUEEN, KING -> {
                 return 10;
             }
